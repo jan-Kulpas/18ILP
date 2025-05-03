7c877bc20cb635e2627c7fc31fd2671b2e419189
jan-Kulpas <102905054+jan-Kulpas@users.noreply.github.com>
2025-05-03 10:23:57 +0200

    split build_graph into more managable functions

--- solver/pathfinder.py
+++ solver/pathfinder.py
@@ -38,90 +38,98 @@ class Pathfinder:
 
         print(f"Finding best route for {railway_id} - Trains: {railway.trains}")
 
-        self.build_graph(railway)
+        self._build_graph(railway)
+        
+        print(len(self.nodes))
+        print(len(self.edges))
 
         problem = LpProblem("MaximalRouteFinding", const.LpMaximize)
 
-    def build_graph(self, railway: Railway) -> None:
-        # The nullcheck takes care of None type
-        home_hexes: list[tuple[Hex, SettlementLocation]] = [
-            (hex, tile.get_station_location(railway))
-            for hex, tile in self.game.board.items()
-            if tile.has_station(railway)
-        ]  # type: ignore
-
-        queue: deque[tuple[str, str]] = deque(
-            [(f"{hex}.{loc.name}", "city") for hex, loc in home_hexes]
-        )
-
+    def _build_graph(self, railway: Railway) -> None:
         # Reset graph
         self.nodes: set[str] = set()
         self.edges: set[tuple[str, str]] = set()
 
+        queue: deque[tuple[str, str]] = deque(
+            [(id, "city") for id in self._get_station_segment_ids(railway)]
+        )
+
         while queue:
             node, kind = queue.popleft()
 
+            if node in self.nodes:
+                continue  # Skip already visited
+
+            self.nodes.add(node)
+            
             match kind:
                 case "city":
-                    print(f"Visiting city: {node}")
-                    self.nodes.add(node)  # mark visited
-                    hex = Hex.from_string(node.split(".")[0])
-
-                    # TODO: check if blocked
-
-                    # Add outgoing edge to every junction and queue that junction
-                    for dir in self.game.board.segment_at(node).tracks:
-                        neighbour = hex.neighbour(dir)
-                        junction = f"{hex}-{neighbour}" if str(hex) < str(neighbour) else f"{neighbour}-{hex}"
-
-                        if junction not in self.nodes:
-                            queue.append((junction, "junction"))
-                        self.edges.add((node, junction))
-
+                    self._process_city(node, queue)
                 case "junction":
-                    print(f"Visiting junction: {node}")
-                    self.nodes.add(node)
+                    self._process_junction(node, queue)
 
-                    # Get the Hexes neighbouring with the junction
-                    hexes = [Hex.from_string(coord) for coord in node.split("-")]
-                    from_hex = hexes[0]
-                    to_hex = hexes[1]
+    def _get_station_segment_ids(self, railway: Railway) -> list[str]:
+        # The nullcheck takes care of None type
+        return [
+            self.city_name(hex, tile.get_station_location(railway))  # type: ignore
+            for hex, tile in self.game.board.items()
+            if tile.has_station(railway)
+        ]
 
-                    # Get the actual segments that are connected to the junction
-                    from_segments: list[Segment] = self.game.board[
-                        from_hex
-                    ].segments_with_exit(Direction.from_unit_hex(to_hex - from_hex))
-                    to_segments: list[Segment] = self.game.board[
-                        to_hex
-                    ].segments_with_exit(Direction.from_unit_hex(from_hex - to_hex))
+    def _process_city(self, node: str, queue: deque[tuple[str, str]]) -> None:
+        print(f"Visiting city: {node}")
 
-                    # Pair segment and hex for city id (not converting we need to handle None case)
-                    cities: list[tuple[Hex, Segment]] = []
-                    cities += [(from_hex, seg) for seg in from_segments]
-                    cities += [(to_hex, seg) for seg in to_segments]
+        hex = Hex.from_string(node.split(".")[0])
 
-                    # Go through every neighbouring thing and add edge to list and thing to queue
-                    for hex, seg in cities:
-                        if seg.location:
-                            city = f"{hex}.{seg.location.name}"
-                            if city not in self.nodes:
-                                queue.append((city, "city"))
-                            self.edges.add((node, city))
-                        else:
-                            # Oops! There's no city actually so we need to make an edge to the next junction.
-                            # I am not sure if there can be multiple directions but just in case.
-                            directions = [
-                                dir
-                                for dir in seg.tracks
-                                if hex.neighbour(dir) not in [from_hex, to_hex]
-                            ]
-                            for dir in directions:
-                                neighbour = hex.neighbour(dir)
-                                junction = f"{hex}-{neighbour}" if str(hex) < str(neighbour) else f"{neighbour}-{hex}"
-                                if junction not in self.nodes:
-                                    queue.append((junction, "junction"))
-                                self.edges.add((node, junction))
+        # TODO: check if blocked
 
+        # Add outgoing edge to every junction and queue that junction
+        for direction in self.game.board.segment_at(node).tracks:
+            neighbour = hex.neighbour(direction)
+            junction = self.junction_name(hex, neighbour)
+
+            self.edges.add((node, junction))
+            queue.append((junction, "junction"))
+
+    def _process_junction(self, node: str, queue: deque[tuple[str,str]]) -> None:
+        print(f"Visiting junction: {node}")
+
+        def get_connected_segments(base_hex: Hex, other_hex: Hex) -> list[tuple[Hex, Segment]]:
+            direction = Direction.from_unit_hex(other_hex - base_hex)
+            return [(base_hex, seg) for seg in self.game.board[base_hex].segments_with_exit(direction)]
+
+        # Get the Hexes neighbouring with the junction
+        hexes = [Hex.from_string(coord) for coord in node.split("-")]
+        from_hex, to_hex = hexes
+
+        # Get the actual segments that are connected to the junction
+        # Pair segment and hex for city id (not converting we need to handle None case)
+        cities: list[tuple[Hex, Segment]] = get_connected_segments(from_hex, to_hex) + get_connected_segments(to_hex, from_hex)
+
+        # Go through every neighbouring thing and add edge to list and thing to queue
+        for hex, seg in cities:
+            if seg.location:
+                city = f"{hex}.{seg.location.name}"
+                queue.append((city, "city"))
+                self.edges.add((node, city))
+            else:
+                # Oops! There's no city actually so we need to make an edge to the next junction.
+                # I am not sure if there can be multiple directions but just in case.
+                directions = [
+                    dir for dir in seg.tracks
+                    if hex.neighbour(dir) not in [from_hex, to_hex]
+                ]
+                for dir in directions:
+                    neighbour = hex.neighbour(dir)
+                    junction = self.junction_name(hex, neighbour)
+                    queue.append((junction, "junction"))
+                    self.edges.add((node, junction))
+
+    def city_name(self, h: Hex, l: SettlementLocation) -> str:
+        return f"{h}.{l.name}"
+
+    def junction_name(self, h1: Hex, h2: Hex) -> str:
+        return f"{h1}-{h2}" if str(h1) < str(h2) else f"{h2}-{h1}"
 
 if __name__ == "__main__":
     game = Game("1889")
